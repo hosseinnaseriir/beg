851f3116640bcf6fe2c64393a3781de259bbd5ab
Hossein Naseri <hossein@ethicadata.com>
2022-06-18 16:10:46 +0430

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  Dimensions,
+} from "react-native";
+
+import { Pedometer } from "expo-sensors";
 
 export default function App() {
+  const [PedomaterAvailability, SetPedomaterAvailability] = useState("");
+  const [StepCount, SetStepCount] = useState(0);
+
+  var WindowHeight = Dimensions.get("window").height;
+
+  var Dist = StepCount / 1300;
+
+  var DistanceCovered = Dist.toFixed(4);
+
+  var cal = DistanceCovered * 60;
+
+  var caloriesBurnt = cal.toFixed(4);
+
+  React.useEffect(() => {
+    subscribe();
+  }, []);
+
+  subscribe = () => {
+    const subscription = Pedometer.watchStepCount((result) => {
+      SetStepCount(result.steps);
+    });
+
+    Pedometer.isAvailableAsync().then(
+      (result) => {
+        SetPedomaterAvailability(String(result));
+      },
+
+      (error) => {
+        SetPedomaterAvailability(error);
+      }
+    );
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        style={{ flex: 1 }}
+        source={require("./assets/splash.png")}
+        resizeMode="cover"
+      >
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <Text style={styles.headingDesign}>
+            Is Pedometer available on the device : {PedomaterAvailability}
+          </Text>
+        </View>
+
+        <View style={{ flex: 3 }}>
+          <Text style={styles.headingDesign}>{StepCount}</Text>
+        </View>
+
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <View style={{ flex: 1 }}>
+            <Text
+              style={[
+                styles.textDesign,
+
+                { paddingLeft: 20, marginLeft: "23%" },
+              ]}
+            >
+              Target : 6500 steps(5kms)
+            </Text>
+          </View>
+
+          <View style={{ flex: 1 }}>
+            <Text
+              style={[
+                styles.textDesign,
+
+                { width: "93%", paddingLeft: 20, marginLeft: "-3.5%" },
+              ]}
+            >
+              Distance Covered : {DistanceCovered} km
+            </Text>
+          </View>
+
+          <View style={{ flex: 1 }}>
+            <Text
+              style={[
+                styles.textDesign,
+
+                { paddingLeft: 10, marginLeft: "23%" },
+              ]}
+            >
+              Calories Burnt : {caloriesBurnt}
+            </Text>
+          </View>
+
+          <StatusBar style="auto" />
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +109,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+    backgroundColor: "#fff",
+  },
+
+  headingDesign: {
+    backgroundColor: "rgba(155, 89, 182,0.5)",
+
+    alignSelf: "center",
+
+    fontSize: 20,
+
+    color: "white",
+
+    fontWeight: "bold",
+  },
+
+  textDesign: {
+    backgroundColor: "rgba(155, 89, 182,0.5)",
+
+    height: 50,
+
+    width: "85%",
+
+    borderColor: "white",
+
+    borderWidth: 1,
+
+    borderRadius: 20,
+
+    overflow: "hidden",
+
+    fontSize: 25,
+
+    color: "white",
+
+    fontWeight: "bold",
   },
 });
